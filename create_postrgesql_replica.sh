8a764fa683f6b1cda910511a175f1378078f61d5
VASYL MELNYK <basil@vpm.net.ua>
2022-05-07 10:06:50 +0300

    Update create_postrgesql_replica.sh

--- create_postrgesql_replica.sh
+++ create_postrgesql_replica.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
-PG_VERSION=12
-PG_MASTER_HOST=db
+# on PG_MASTER_HOST
+# create replication user
+# sudo su - postgres -c "psql CREATE USER $PG_REPLICATION_USER WITH REPLICATION ENCRYPTED PASSWORD '$PG_REPLICATION_USER_PASSWORD';"
+# add to pg_hba.conf
+# host replication $PG_REPLICATION_USER $PG_REPLICATION_HOST_ADDRESS md5
+
+PG_VERSION=11
+PG_MASTER_HOST=db65
 PG_MASTER_HOST_PORT=5432
-PG_REPLICATION_USER=repluser
+PG_REPLICATION_USER=replication
 PG_REPLICATION_USER_PASSWORD=password
 PG_DATA_DIRECTORY=/var/lib/pgsql
 
-
-# Stop postgresql
 systemctl stop postgresql-${PG_VERSION}.service
 
-# CLean postgresql data directory
 rm -rf $PG_DATA_DIRECTORY/$PG_VERSION/data/*
 
-# Create pgpass
 sudo su - postgres -c "echo $PG_MASTER_HOST:$PG_MASTER_HOST_PORT:*:$PG_REPLICATION_USER:$PG_REPLICATION_USER_PASSWORD > .pgpass"
 sudo su - postgres -c "chmod 600 .pgpass"
 
-# Copy database from master
-sudo su - postgres -c "pg_basebackup -h $PG_MASTER_HOST -p $PG_MASTER_HOST_PORT -U $PG_REPLICATION_USER -D $PG_DATA_DIRECTORY/$PG_VERSION/data --checkpoint=fast --wal-method=fetch
+sudo -iu postgres pg_basebackup -h $PG_MASTER_HOST -p $PG_MASTER_HOST_PORT -U $PG_REPLICATION_USER -D $PG_DATA_DIRECTORY/$PG_VERSION/data --checkpoint=fast --wal-method=fetch -R -P -w
 
-# Create recovery.conf for old postgresql
-if [ $PG_VERSION -lt 12 ]; then
-    sudo su - postgres -c "echo trigger_file = $PG_DATA_DIRECTORY/$PG_VERSION/data/trigger_file >> $PG_DATA_DIRECTORY/$PG_VERSION/data/recovery.conf"
-fi
+echo -e "trigger_file = '$PG_DATA_DIRECTORY/$PG_VERSION/data/trigger_file'" >> $PG_DATA_DIRECTORY/$PG_VERSION/data/recovery.conf
 
-# Start postgresql
 systemctl start postgresql-${PG_VERSION}.service
