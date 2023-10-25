0e8b09ec67d2c77a560c8c10fccb3e2dadc11abf
VASYL MELNYK <basil@vpm.net.ua>
2023-10-25 22:08:01 +0300

    Update dovecot.sh

--- debian/letsencrypt/renewal-hooks/post/dovecot.sh
+++ debian/letsencrypt/renewal-hooks/post/dovecot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-install --no-target-directory --mode=600 --owner=dovecot --group=dovecot  /etc/letsencrypt/live/mail.zkr.com.ua/cert.pem /etc/dovecot/private/mail.zkr.com.ua.cert.pem
-install --no-target-directory --mode=600 --owner=dovecot --group=dovecot  /etc/letsencrypt/live/mail.zkr.com.ua/privkey.pem /etc/dovecot/private/mail.zkr.com.ua.privkey.pem
+install --no-target-directory --mode=600 --owner=dovecot --group=dovecot  /etc/letsencrypt/live/mail.vpm.net.ua/cert.pem /etc/dovecot/private/mail.vpm.net.ua.cert.pem
+install --no-target-directory --mode=600 --owner=dovecot --group=dovecot  /etc/letsencrypt/live/mail.vpm.net.ua/privkey.pem /etc/dovecot/private/mail.vpm.net.ua.privkey.pem
 systemctl reload dovecot.service
