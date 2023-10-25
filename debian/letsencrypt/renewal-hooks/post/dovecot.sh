7aeafb66d780d3619e8ea9286802cbe39be4ef3a
VASYL MELNYK <basil@vpm.net.ua>
2023-10-25 22:10:06 +0300

    Delete debian directory

--- debian/letsencrypt/renewal-hooks/post/dovecot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-install --no-target-directory --mode=600 --owner=dovecot --group=dovecot  /etc/letsencrypt/live/mail.vpm.net.ua/cert.pem /etc/dovecot/private/mail.vpm.net.ua.cert.pem
-install --no-target-directory --mode=600 --owner=dovecot --group=dovecot  /etc/letsencrypt/live/mail.vpm.net.ua/privkey.pem /etc/dovecot/private/mail.vpm.net.ua.privkey.pem
-systemctl reload dovecot.service
--- debian/letsencrypt/renewal-hooks/post/exim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-install --no-target-directory --mode=600 --owner=Debian-exim --group=Debian-exim  /etc/letsencrypt/live/mail.vpm.net.ua/cert.pem /etc/exim4/mail.vpm.net.ua.cert.pem
-install --no-target-directory --mode=600 --owner=Debian-exim --group=Debian-exim  /etc/letsencrypt/live/mail.vpm.net.ua/privkey.pem /etc/exim4/mail.vpm.net.ua.privkey.pem
-systemctl reload exim4.service
